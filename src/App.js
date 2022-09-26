b83d3c1770dcece6e220c7808a569d8a77f15b5d
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-26 18:52:50 +0200

    A simple header component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
-      <h1 className='text-5xl text-center'>Hello</h1>
+      <Header></Header>
     </div>
   );
 }
